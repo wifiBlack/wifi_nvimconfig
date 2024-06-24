d66f012b2b31a599c055c9ac076bb155ce311023
wifi <906717196@qq.com>
2024-06-24 20:03:07 +0800

    not much

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,10 +10,10 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pyright'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+  ensure_installed = {'pyright', 'lua_ls'},
+  -- handlers = {
+    -- function(server_name)
+      -- require('lspconfig')[server_name].setup({})
+    -- end,
+  -- },
 })
