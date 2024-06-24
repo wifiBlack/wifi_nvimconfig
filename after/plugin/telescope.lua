97963679792b04de46063ba7a8d25c81f2f1b486
wifi <906717196@qq.com>
2024-06-24 20:30:25 +0800

    try to commit

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', 'C-g>', builtin.git_files, {})
