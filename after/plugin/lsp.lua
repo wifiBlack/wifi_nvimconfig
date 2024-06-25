80ba0d302d96a08377457d6ff99f876a2a99fae5
wifi <906717196@qq.com>
2024-06-25 14:53:46 +0800

    init

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,27 +15,3 @@ require('mason-lspconfig').setup({
   },
 })
 
--- 设置 nvim-cmp 补全
-local cmp = require'cmp'
-cmp.setup({
-  mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-  },
-})
