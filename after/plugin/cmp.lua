bd28b7520ff6da55d64dcffe67db3ecf58979cc2
wifi <906717196@qq.com>
2024-06-25 11:31:58 +0800

    modify lsp,add cmp.lua

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,20 +1,3 @@
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-  -- 设定默认 LSP 快捷键
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'pyright', 'lua_ls'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
-})
-
 -- 设置 nvim-cmp 补全
 local cmp = require'cmp'
 cmp.setup({
