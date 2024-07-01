4e532e46a162748f528b75dc87e72fc2d4ba1513
wifi <906717196@qq.com>
2024-07-01 10:15:03 +0800

    update

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,24 +1,24 @@
 -- 加载 nvim-dap 和 nvim-dap-python 插件
-local dap = require('dap')
-local dap_python = require('dap-python')
+local dap = require("dap")
+local dap_python = require("dap-python")
 
 -- 设置 Python 调试器路径（修改为你系统上的路径）
-dap_python.setup('~/anaconda3/envs/openmmlab/bin/python')
+dap_python.setup("~/anaconda3/envs/openmmlab/bin/python")
 
 -- 设置断点样式
-vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
-vim.fn.sign_define('DapStopped', {text='➡️', texthl='', linehl='', numhl=''})
+vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "" })
+vim.fn.sign_define("DapStopped", { text = "➡️", texthl = "", linehl = "", numhl = "" })
 
-require('dapui').setup()
+require("dapui").setup()
 -- 调试会话打开时自动打开 dapui
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  require('dapui').open()
+	require("dapui").open()
 end
-
--- 调试会话结束时自动关闭 dapui
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  require('dapui').close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  require('dapui').close()
-end
+vim.keymap.set("n", "<leader>de", ':lua require("dapui").close()<CR>', { noremap = true, silent = true })
+-- -- 调试会话结束时自动关闭 dapui
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   require('dapui').close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   require('dapui').close()
+-- end
--- lua/wifi/remap.lua
+++ lua/wifi/remap.lua
@@ -32,37 +32,51 @@ keymap.set("n", "<leader>p", '"+p', { desc = "从系统剪切板粘贴" })
 keymap.set("n", "<leader>das", vim.cmd.Dashboard, { desc = "打开 Dashboard" })
 
 -- 切换到终端1
-vim.api.nvim_set_keymap('n', '<leader>t1', ':ToggleTerm 1<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>t1", ":ToggleTerm 1<CR>", { noremap = true, silent = true })
 -- 切换到终端2
-vim.api.nvim_set_keymap('n', '<leader>t2', ':ToggleTerm 2<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>t2", ":ToggleTerm 2<CR>", { noremap = true, silent = true })
 -- 切换到终端3
-vim.api.nvim_set_keymap('n', '<leader>t3', ':ToggleTerm 3<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>t3", ":ToggleTerm 3<CR>", { noremap = true, silent = true })
 
 -- 水平分割打开终端1
-vim.api.nvim_set_keymap('n', '<leader>th1', ':ToggleTerm 1 direction=horizontal<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>th1", ":ToggleTerm 1 direction=horizontal<CR>", { noremap = true, silent = true })
 -- 垂直分割打开终端2
-vim.api.nvim_set_keymap('n', '<leader>tv2', ':ToggleTerm 2 direction=vertical<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tv2", ":ToggleTerm 2 direction=vertical<CR>", { noremap = true, silent = true })
 -- 浮动窗口打开终端3
-vim.api.nvim_set_keymap('n', '<leader>tf3', ':ToggleTerm 3 direction=float<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tf3", ":ToggleTerm 3 direction=float<CR>", { noremap = true, silent = true })
 
 -- 调试控制快捷键
 
 -- 调试控制快捷键，加入 CTRL 组合键
-vim.api.nvim_set_keymap('n', '<leader>db', ':lua require"dap".continue()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>nn', ':lua require"dap".step_over()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-F11>', ':lua require"dap".step_into()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-F12>', ':lua require"dap".step_out()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>b', ':lua require"dap".toggle_breakpoint()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>B', ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>lp', ':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>dr', ':lua require"dap".repl.open()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>dl', ':lua require"dap".run_last()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>db", ':lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>nn", ':lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-F11>", ':lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-F12>", ':lua require"dap".step_out()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>b",
+	':lua require"dap".toggle_breakpoint()<CR>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>B",
+	':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>lp",
+	':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>',
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<Leader>dr", ':lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dl", ':lua require"dap".run_last()<CR>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('i', '<C-Tab>', 'codeium#Accept()', { noremap = true, expr = true, silent = true })  
-
+vim.api.nvim_set_keymap("i", "<C-Tab>", "codeium#Accept()", { noremap = true, expr = true, silent = true })
 
 -- 跳转到函数定义
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
 -- 查看函数定义信息（浮动窗口）
-vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
